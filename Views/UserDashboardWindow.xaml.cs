9c349f8d948628a1c8b490bdb54a6a183f814fc2
lenny <leonid.gevorgyan42@gmail.com>
2024-05-19 00:39:40 +0700

    feat: switching between tabs

--- Views/UserDashboardWindow.xaml.cs
+++ Views/UserDashboardWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FintechBank.Views.ViewsPages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,107 @@ namespace FintechBank.Views
         public UserDashboardWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new WalletPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void WalletButton_Click(object sender, RoutedEventArgs e)
+        {
+            WalletButton.Style = (Style)FindResource("activeMenuButton");
+            WalletButtonArrow.Visibility = Visibility.Visible;
+
+            if (WalletButtonArrow.Visibility == Visibility.Visible)
+            {
+                DepositsButtonArrow.Visibility = Visibility.Hidden;
+                HistoryButtonArrow.Visibility = Visibility.Hidden;
+                SettingsButtonArrow.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                WalletButtonArrow.Visibility = Visibility.Hidden;
+            }
+
+            HistoryButton.Style = (Style)FindResource("menuButton");
+            DepositsButton.Style = (Style)FindResource("menuButton");
+            SettingsButton.Style = (Style)FindResource("menuButton");
+            MainFrame.Navigate(new WalletPage());
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            HistoryButton.Style = (Style)FindResource("activeMenuButton");
+            HistoryButtonArrow.Visibility = Visibility.Visible;
+
+            if (HistoryButtonArrow.Visibility == Visibility.Visible)
+            {
+                DepositsButtonArrow.Visibility = Visibility.Hidden;
+                WalletButtonArrow.Visibility = Visibility.Hidden;
+                SettingsButtonArrow.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                HistoryButtonArrow.Visibility = Visibility.Hidden; 
+            }
+
+            WalletButton.Style = (Style)FindResource("menuButton");
+            DepositsButton.Style = (Style)FindResource("menuButton");
+            SettingsButton.Style = (Style)FindResource("menuButton");
+            MainFrame.Navigate(new HistoryPage());
+        }
+
+        private void DepositsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            DepositsButton.Style = (Style)FindResource("activeMenuButton");
+            DepositsButtonArrow.Visibility = Visibility.Visible;
+
+            if (DepositsButtonArrow.Visibility == Visibility.Visible)
+            {
+                WalletButtonArrow.Visibility = Visibility.Hidden;
+                HistoryButtonArrow.Visibility = Visibility.Hidden;
+                SettingsButtonArrow.Visibility = Visibility.Hidden;
+            } else
+            {
+                DepositsButtonArrow.Visibility = Visibility.Hidden;
+            }
+
+            HistoryButton.Style = (Style)FindResource("menuButton");
+            WalletButton.Style = (Style)FindResource("menuButton");
+            SettingsButton.Style = (Style)FindResource("menuButton");
+            MainFrame.Navigate(new DepositsPage());
+        }
+
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsButton.Style = (Style)FindResource("activeMenuButton");
+            SettingsButtonArrow.Visibility = Visibility.Visible;
+
+
+            if (SettingsButtonArrow.Visibility == Visibility.Visible)
+            {
+                DepositsButtonArrow.Visibility = Visibility.Hidden;
+                WalletButtonArrow.Visibility = Visibility.Hidden;
+                HistoryButtonArrow.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                SettingsButton.Visibility = Visibility.Hidden;
+                
+            }
+
+            HistoryButton.Style = (Style)FindResource("menuButton");
+            WalletButton.Style = (Style)FindResource("menuButton");
+            DepositsButton.Style = (Style)FindResource("menuButton");
+            MainFrame.Navigate(new SettingsPage());
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
